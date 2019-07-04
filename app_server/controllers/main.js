b2d5634193d8ef41075f17abb5bbff383e0be915
FelipeAzucares <phil@red-robot.biz>
2019-07-04 18:22:07 +0100

    problems with telephone and email being overwritten

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,22 +3,19 @@ const reader = require('../controllers/readFile');
 const parser = require('../controllers/parseFile');
 const fs = require('fs')
 
-
-
 const index = (req, res) => {
     reader.getFileAsync('./app_server/models/Agents_2018.txt').then(dataOutput => {
         console.log('Got data. Serving page');
         // dataOutput = JSON.stringify(dataOutput.split('\t\r\n'));
         dataOutput = dataOutput.split('\t\r\n');       
         //console.log(JSON.stringify(dataOutput));
-        parser.parseFile(dataOutput.slice(0,16));
-        res.render('index',{title:'Agents', agentData: dataOutput});
+        parsedData = parser.parseFile(dataOutput.slice(0,16));
+        res.render('index',{title:'Agents', agentData: parsedData});
     }).catch(err => {
         console.error(err);
     });
 
   };
-
 
 module.exports={
     index
--- app_server/controllers/parseFile.js
+++ app_server/controllers/parseFile.js
@@ -3,7 +3,7 @@
     and constructs a javascript object
 */
 
-const parseFile = function (inputDataRecs) {
+const parseFile = (inputDataRecs) => {
     let accumulatedData = '';
     let parsedRecords = [{}];
     // these are fields that we are looking for in each input element
@@ -15,42 +15,38 @@ const parseFile = function (inputDataRecs) {
         'back to explore categories',
         'Directors']
 
-    let recordStart = true;
-    let index = 0;
+    let index = -1;
     let currentField = 'unknown';
     let found = false;
     let debug = true;
-
-    // need to add in logic to extract name from first couple of records keying
-    // on 'back to explore' and then generate a logical record for each 
-    // records post 'back to explore  should be added into a 'details' category
-    // once this is structured we can write it into a database file
 
     for (i = 0; i <= inputDataRecs.length - 1; i++) {
         found = false;
         /* check current record against list of categories */
         for (j = 0; j <= categories.length - 1; j++) {
             if (debug == true) { console.log(`Looking for ${categories[j]} `) }
-            if (inputDataRecs[i].toUpperCase().trim() == categories[j].toUpperCase()) {
+            if (inputDataRecs[i].toLowerCase().trim() == categories[j].toLowerCase()) {
                 // detected new field name so write accumuated data to parsed records with last key read from file 
                 // first of all check to see if we've detected back to explore which is start of a new agency
                 found = true;
                 if (categories[j] != 'back to explore categories') {
                     recordStart = false;
-                    if (debug == true) { console.log(`Found in ${inputDataRecs[i]}`) }
+                    if (debug == true) { console.log(`Found ${categories[j]} in ${inputDataRecs[i]}`) }
                     parsedRecords[index][currentField] = accumulatedData;
                     //now reset name of field we are collecting data for
-                    currentField = categories[j];
+                    currentField = categories[j].toLowerCase();
                     //once we've found a category don't need to search for any more
                     break;
                 }
                 else {
-                    if (debug == true) { console.log(`Found new agency ${inputDataRecs[i]}`) }
+                    if (debug == true) { console.log(`Found new agency ${inputDataRecs[i]}: data accumulated: ${accumulatedData}`) }
                     index += 1;
                     //currentField = "name";
-                    parsedRecords.push({name: accumulatedData});
+                    //parsedRecords[index]['name'] = accumulatedData
+                    parsedRecords[index]['name'] = accumulatedData;
                     //after the name we usually get the agencies' deets
-                    currentField = "details;"
+                    currentField = "details";
+
                 }
                 //reset accumulated data for the field we are now collecting for
                 accumulatedData = '';
@@ -64,6 +60,7 @@ const parseFile = function (inputDataRecs) {
     }
 
     if (debug == true) { console.log(`result: ${JSON.stringify(parsedRecords)}`) }
+    return parsedRecords;
 }
 
 module.exports = {
