1a256affc3006e69548f35f7384b42b38d965058
FelipeAzucares <phil@red-robot.biz>
2019-07-04 17:37:21 +0100

    parseFile detects new agencies

--- app_server/controllers/parseFile.js
+++ app_server/controllers/parseFile.js
@@ -30,29 +30,40 @@ const parseFile = function (inputDataRecs) {
         found = false;
         /* check current record against list of categories */
         for (j = 0; j <= categories.length - 1; j++) {
-            if(debug == true){console.log(`Looking for ${categories[j]} `)}
+            if (debug == true) { console.log(`Looking for ${categories[j]} `) }
             if (inputDataRecs[i].toUpperCase().trim() == categories[j].toUpperCase()) {
                 // detected new field name so write accumuated data to parsed records with last key read from file 
-                recordStart = false;
+                // first of all check to see if we've detected back to explore which is start of a new agency
                 found = true;
-                if(debug == true){console.log(`Found in ${inputDataRecs[i]}`)}
-                parsedRecords[index][currentField] = accumulatedData;
-                //now reset name of field we are collecting data for
-                currentField = categories[j];
+                if (categories[j] != 'back to explore categories') {
+                    recordStart = false;
+                    if (debug == true) { console.log(`Found in ${inputDataRecs[i]}`) }
+                    parsedRecords[index][currentField] = accumulatedData;
+                    //now reset name of field we are collecting data for
+                    currentField = categories[j];
+                    //once we've found a category don't need to search for any more
+                    break;
+                }
+                else {
+                    if (debug == true) { console.log(`Found new agency ${inputDataRecs[i]}`) }
+                    index += 1;
+                    //currentField = "name";
+                    parsedRecords.push({name: accumulatedData});
+                    //after the name we usually get the agencies' deets
+                    currentField = "details;"
+                }
                 //reset accumulated data for the field we are now collecting for
                 accumulatedData = '';
-                //once we've found a category don't need to search for any more
-                break;
             }
         }
         //only accumulate for non empty records or ones that don't contain categories
         if (inputDataRecs[i].length != 0 && found != true) {
-            if(debug == true){console.log(`accumulating: ${currentField} : ${inputDataRecs[i]} `)}
+            if (debug == true) { console.log(`accumulating: ${currentField} : ${inputDataRecs[i]} `) }
             accumulatedData += inputDataRecs[i]
         }
     }
 
-    if(debug == true){console.log(`result: ${JSON.stringify(parsedRecords)}`)}
+    if (debug == true) { console.log(`result: ${JSON.stringify(parsedRecords)}`) }
 }
 
 module.exports = {
